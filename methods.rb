3c2436c7be804130fce99e71846e19825906bd24
charity <wa.nelimacharity@gmail.com>
2022-10-20 16:15:36 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer;
+ puts "Hello, programmer!"
+
+end
+
+def greet (name = "Naureen");
+ puts "Hello, #{name}!"
+
+end
+
+#def greet_with_default () ;
+ #puts "Hello, programmer!"
+# puts "Hello, Naureen!"
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+
+
+end
+
+def add (num1, num2)
+    return num1 + num2
+  end
+
+#def  halve (num3, num4)
+   # return num3 / num4
+
+#end
+def halve(num)
+    if(num.class != Integer)
+        return nil
+    end
+    num/2
+end
